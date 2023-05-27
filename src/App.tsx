6de5556de19b86b2778bd6b8adf86a6192b28f13
haybuka <paschal.nduleo@gmail.com>
2023-05-27 20:18:51 +0100

    updating objects in app

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,35 @@ import Button from "./components/Button";
 function App() {
   const items = ["New York", "London", "Paris"];
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [drink, setDrink] = useState({ title: "Americano", price: 5 });
+  const [customer, setCustomer] = useState({
+    name: "Johnero",
+    price: 5,
+    address: { city: "Francisco" , zipCode : 1},
+  });
   const handleSelectItem = (item: string) => {
     alert(item);
   };
 
   const handleAlert = () => {
     setIsOpen((previous) => !previous);
+  };
+  const handleCustomerChange = () => {
+    setCustomer({
+      ...customer,
+      address: {
+        ...customer.address,
+        zipCode: 988,
+      },
+    });
+  };
+  const handleDrinkIncrease = () => {
+    //adding new drink
+    const newDrink = {
+      ...drink,
+      price: drink.price + 1,
+    };
+    setDrink(newDrink);
   };
   return (
     <div className="App">
@@ -26,6 +49,11 @@ function App() {
       <Button color="danger" onClick={handleAlert}>
         {isOpen ? "Close Alert" : "Open Alert"}
       </Button>
+
+      <p onClick={handleDrinkIncrease}>
+        <span>{drink.title}</span>
+        <span>{drink.price}</span>
+      </p>
     </div>
   );
 }
