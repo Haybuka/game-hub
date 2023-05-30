e33885daca3ebcdf28dd14fc450724bbe1fbb126
haybuka <paschal.nduleo@gmail.com>
2023-05-30 18:42:06 +0100

    updated title

--- src/components/gameGrid/gameGrid.tsx
+++ src/components/gameGrid/gameGrid.tsx
@@ -13,9 +13,10 @@ interface Props {
 const GameGrid = ({gameQuery}:Props) => {
   const { data:games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+  if(error) return  <Text>{error}</Text>
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
+  
       <SimpleGrid
         padding={5}
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
@@ -33,7 +34,7 @@ const GameGrid = ({gameQuery}:Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
+    
   );
 };
 
